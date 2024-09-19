9a696f9ebc8ea3899c5a52800bf6cffc9a584e58
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-09-19 15:14:51 -0700

    Fixed loop logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,28 @@
-let response = prompt(`Which planet in our solar system has the most moons?`);
-response = response.toLowerCase;
+let response;
+let hints = [
+    `It is a gas giant known for its rings.`,
+    `This planet is the second-largest in the solar system.`,
+    `It has over 80 moons, including one named Titan.`,
+    `Its rings are made of ice and rock.`,
+    `It is the sixth planet from the Sun.`
+]
+do {
+    response = prompt(`Which planet in our solar system has the most moons?`)
+} while (response == ``);
+
 if (response) {
-    if (response == 'saturn'){
-        alert(`That is correct!`);
-    } else {alert(`That is incorrect!`)};
-    
+    if (response.toLowerCase() == 'saturn'){
+        alert(`Good job! That is correct! The planet with the most moons is Saturn. It has 83 confirmed moons, including the famous Titan, and is known for its iconic ring system.`);
+    } else {
+        for (let i = 0; i < hints.length; i++) {
+            response = prompt(`That is incorrect... Please try again. ${hints[i]}`);
+            if (response.toLowerCase() == 'saturn'){
+                alert(`Good job! That is correct! The planet with the most moons is Saturn. It has 83 confirmed moons, including the famous Titan, and is known for its iconic ring system.`);
+                break;
+            } 
+        };
+        if (response.toLowerCase() !== 'saturn'){
+            alert(`GAME OVER. Please brush up on your planetary knowledge. The planet with the most moons is Saturn. It has 83 confirmed moons, including the famous Titan, and is known for its iconic ring system.`)
+        }
+    }
 }
-
-
-// let response = prompt(`What is your name?`);
-// let namenode = document.getElementById(`name`);
-// let heading = document.getElementsByTagName(`h1`);
-// //confirm allows us to gather a true/false response from users using a popup
-// if (response) { //if there is a response
-//     let age = confirm(`${response} are you over the age of 18?`);
-//     if (age) {
-//         namenode.textContent = response;
-//     } else {
-//         heading[0].textContent = `Warning you are a child, out otta here!`
-//         //alert(`WARNING: You are a child, get otta here!`) //This works too, but page still loads after clicking ok
-//     }
-// }
